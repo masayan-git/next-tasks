dacb5a82715a2beec5ae8edd08e3e8d2fd3d1144
masayan-git <68589347+masayan-git@users.noreply.github.com>
2024-09-18 23:11:42 +0900

    first commit

---
+++ src/components/SideMenu/NavList/NavList.tsx
@@ -0,0 +1,42 @@
+import { FaRegCheckSquare, FaRegClock, FaTasks } from "react-icons/fa";
+import NavItem from "./NavItem/NavItem";
+
+interface NavItemType {
+  id: number;
+  label: string;
+  link: string;
+  icon: React.ReactNode;
+}
+
+const NavList = () => {
+  const navList: NavItemType[] = [
+    {
+      id: 1,
+      label: "All tasks",
+      link: "/",
+      icon: <FaTasks className="size-5" />,
+    },
+    {
+      id: 2,
+      label: "Completed tasks",
+      link: "/completed",
+      icon: <FaRegCheckSquare className="size-5" />,
+    },
+    {
+      id: 3,
+      label: "Expired tasks",
+      link: "/expired",
+      icon: <FaRegClock className="size-5" />,
+    },
+  ];
+
+  return (
+    <div className="mt-24">
+      {navList.map((item) => (
+        <NavItem key={item.id} label={item.label} link={item.link} icon={item.icon}/>
+      ))}
+    </div>
+  );
+};
+
+export default NavList;
